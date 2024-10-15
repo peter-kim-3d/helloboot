e0c0496d5a07c7b12e4230c76b5874781586b482
peter-kim-3d <peter.kim.3d@gmail.com>
2024-10-15 10:55:07 -0400

    add print

--- src/main/java/tobyspring/config/MyOnClassCondition.java
+++ src/main/java/tobyspring/config/MyOnClassCondition.java
@@ -3,6 +3,7 @@ package tobyspring.config;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.util.ClassUtils;
 
 import java.util.Map;
@@ -12,6 +13,17 @@ public class MyOnClassCondition implements Condition {
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         Map<String, Object> attrs = metadata.getAnnotationAttributes(ConditionalMyOnClass.class.getName());
         String value = (String) attrs.get("value");
+        System.out.println("MyOnClassCondition: " + value);
+        if(metadata instanceof AnnotationMetadata) {
+            AnnotationMetadata annotationMetadata = (AnnotationMetadata) metadata;
+            String className = annotationMetadata.getClassName();
+            try {
+                Class<?> aClass = Class.forName(className);
+                System.out.println("------------>" + aClass.getName());
+            } catch ( Exception e) {
+                e.printStackTrace();
+            }
+        }
         return ClassUtils.isPresent(value, context.getClassLoader());
     }
 }
